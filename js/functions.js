32dc87b32a82096e12bb8e8f7d726646d96c1e37
Flo <florian.wirt@web.de>
2020-07-12 14:23:48 +0200

    updates

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,59 @@
 		(function ($) {
 	  $(document).ready(function(){
 
+		AOS.init();
+
+		
+
+
+		$('.fa-plus').click(function() {
+
+			$('.fa-phone').toggleClass('active');
+			$('.fa-plus').toggleClass('active');
+			$('.fa-whatsapp').toggleClass('active');
+			$('.fa-envelope').toggleClass('active');
+			$('.fa-calendar-check').toggleClass('active');
+			
+
+		});
+
+		
+
+			
+/* 
+			$('.stickycontact .fa-phone').css({"background-color": "#fd7e14", 
+			"transform": "translateX(-90px)",  
+				
+			
+
+/* 			$('.fa-envelope').css({
+	
+				"transform": "translateX(-360px)",
+				"visibility": "visible",
+			})
+
+			$('.fa-whatsapp').css({
+				"transform": "translateX(-180px)",
+				"visibility": "visible",
+			})
+
+			$('.stickycontact .fa-calendar-check').css({
+				"transform": "translateX(-270px)",
+				"visibility": "visible",
+			})
+
+			$('.fa-times').css({
+	
+				"visibility": "visible",
+			}) */
+
+
+
+
+		
+	
+	
+
 
 		
 		// hide .navbar first
@@ -8,12 +61,18 @@
 		
 		// fade in .navbar
 		$(function () {
+
+			
+
+		
 			$(window).scroll(function () {
 				// set distance user needs to scroll before we fadeIn navbar
 				if ($(this).scrollTop() > 50 ) {
 
 					$('.navbar-brand img').css('height', '35px');
+					$('.navbar-brand img:last-of-type').hide();
 					$('#navbar').addClass('bg-light');
+					$('.stickycontact').fadeIn();
 					
                 //     $('#navbar').fadeIn();
                 //    $('#navbar').css( {
@@ -22,11 +81,23 @@
                 //    })
                      
 				
+				} else if ($(window).width() < 700) {
+
+					$('.navbar-brand img:first-of-type').css('height', '60px');
+					$('.navbar-brand img:last-of-type').show();
+					$('.navbar-brand img:last-of-type').css('height', '35px');
+					$('#navbar').removeClass('bg-light');
+					$('.stickycontact').hide();
+					
+
+
 				} else {
 					//$('#navbar').fadeOut();
 					$('.navbar-brand img:first-of-type').css('height', '100px');
+					$('.navbar-brand img:last-of-type').show();
 					$('.navbar-brand img:last-of-type').css('height', '50px');
-                    $('#navbar').removeClass('bg-light');
+					$('#navbar').removeClass('bg-light');
+					$('.stickycontact').hide();
 				}
 			});
 	
@@ -53,12 +124,14 @@
 			easing: 'easeInOut',
 			duration: 1400,
 			svgStyle: null,
+
 			text: {
 			  value: '',
-			  alignToBottom: false
+			  alignToBottom: false,
+
 			},
 			from: {color: '#FFEA82'},
-			to: {color: '#ED6A5A'},
+			to: {color: '#28a745'},
 			// Set default step function for all animate calls
 			step: (state, bar) => {
 			  bar.path.setAttribute('stroke', state.color);
@@ -73,12 +146,13 @@
 			}
 		  });
 		  bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-		  bar.text.style.fontSize = '2rem';
+		  bar.text.style.fontSize = '4rem';
 		  
 		  bar.animate(0.96);  // Number from 0.0 to 1.0
 	
 	});
-	  }(jQuery));
+		}(jQuery));
+
 	  
 
 // progressbar.js@1.0.0 version is used
